889e24d71ec27e5bbdc80e8802a8357794e18bb6
JaslanD <jaslan.dlnv@gmail.com>
2023-05-17 16:14:58 +0300

    refactor: Chat.tsx

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,36 +1,51 @@
-import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
+import { Container, Grid } from '@mui/material'
 
 import { type FC } from 'react'
 
-import { Header } from 'widget/Header/Header'
+import { AddNumberModal } from 'widgets/AddNumberModal/AddNumberModal'
+import { ChatWindow } from 'widgets/ChatWindow/ChatWindow'
+import { ContactList } from 'widgets/ContactList/ContactList'
+import { Navbar } from 'widgets/Navbar/Navbar'
 
-const Chat: FC = (props) => {
+const classes = {
+  root: {
+    height: '100vh',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  sidebar: {
+    height: '100%',
+    background: '#111B21',
+    borderRight: '1px solid #404040'
+  }
+}
+
+const Chat: FC = () => {
   const backgroundImageUrl = 'url("background.jpg")'
 
   return (
-    <Container
-      maxWidth={false}
-      disableGutters
-      sx={{ height: '100vh', width: '100%', display: 'flex', flexDirection: 'column' }}
-    >
-      <Header />
-      <Grid container sx={{ flexGrow: 1 }}>
-        <Grid item xs={3} sx={{ height: '100%', background: '#111B21' }}>
-          Contact List
+    <>
+      <AddNumberModal />
+      <Container sx={classes.root} maxWidth={false} disableGutters>
+        <Navbar />
+        <Grid container sx={{ flexGrow: 1 }}>
+          <Grid sx={classes.sidebar} item xs={5}>
+            <ContactList />
+          </Grid>
+          <Grid
+            item
+            xs={7}
+            sx={{
+              background: `linear-gradient(to top, rgba(11,20,26, 0.95), rgba(11,20,26, 0.95)), ${backgroundImageUrl}`,
+              backgroundSize: 'contain'
+            }}
+          >
+            <ChatWindow />
+          </Grid>
         </Grid>
-        <Grid
-          item
-          xs={9}
-          sx={{
-            background: `linear-gradient(to top, rgba(11,20,26, 0.9), rgba(11,20,26, 0.9)), ${backgroundImageUrl}`,
-            backgroundSize: 'contain'
-          }}
-        >
-          Chat
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   )
 }
 
