c5dbf793c630e6c970c8d1f58bc25b5c57e1ba36
JaslanD <jaslan.dlnv@gmail.com>
2023-05-17 11:25:14 +0300

    refactor: change eslint && prettier

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,10 +1,14 @@
-import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector
+} from 'react-redux'
 
 import type { RootState, AppDispatch } from './store'
 
 import type { AnyAction, Dispatch, ThunkDispatch } from '@reduxjs/toolkit'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = (): ThunkDispatch<any, undefined, AnyAction> & Dispatch<AnyAction> =>
-  useDispatch<AppDispatch>()
+export const useAppDispatch = (): ThunkDispatch<any, undefined, AnyAction> &
+  Dispatch<AnyAction> => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
