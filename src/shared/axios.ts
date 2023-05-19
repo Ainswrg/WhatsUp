f38c0430a53a58f266db55fc378b822bc4f4f049
JaslanD <jaslan.dlnv@gmail.com>
2023-05-19 12:05:13 +0300

    refactor: add headers in axios

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -3,7 +3,10 @@ import axios from 'axios'
 import { constants } from 'shared/const'
 
 const instance = axios.create({
-  baseURL: constants.BASE_URL
+  baseURL: constants.BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
 export default instance
